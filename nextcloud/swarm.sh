08a2c20a170bdb05b5e00a9040dd8f9a82dafc91
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2016-11-11 14:40:37 +0100

    Updates

--- nextcloud/swarm.sh
+++ nextcloud/swarm.sh
@@ -18,39 +18,42 @@ done
 }
 
 function createDB {
-  echo "Creating DB service ${SERVICES[1]} with volume ${VOLUMES[1]}"
-docker service create --name ${SERVICES[1]} --replicas 1 --network ${NET} \
---mount type=volume,src=${VOLUMES[1]},dst=/var/lib/postgresql/data \
+  echo "Creating DB service ${SERVICES[0]} with volume ${VOLUMES[0]}"
+docker service create --name ${SERVICES[0]} --replicas 1 --network ${NET} \
+--mount type=volume,src=${VOLUMES[0]},dst=/var/lib/postgresql/data \
 --constraint 'node.hostname==sparrow' \
 whatever4711/postgres:armhf
 }
 
 function createRedis {
-docker service create --name ${SERVICES[2]} --replicas 1 --network ${NET} \
+  echo "Creating Redis service ${SERVICES[1]}"
+docker service create --name ${SERVICES[1]} --replicas 1 --network ${NET} \
 --constraint 'node.hostname!=jack' \
 armhf/redis
 }
 
 function createMemcache {
-docker service create --name ${SERVICE[3]} --replicas 1 --network ${NET} \
+  echo "Creating Memcache service ${SERVICES[2]}}"
+docker service create --name ${SERVICE[2]} --replicas 1 --network ${NET} \
 --constraint 'node.hostname!=jack' \
 armhf/memcached:alpine
 }
 
 function createNextCloud {
-docker service create --name ${SERVICES[4]} --replicas 1 --network ${NET} \
+  echo "Creating Memcache service ${SERVICES[3]} with volumes ${VOLUMES[1]}, ${VOLUMES[2]}, ${VOLUMES[3]}, and ${VOLUMES[4]}"
+docker service create --name ${SERVICES[3]} --replicas 1 --network ${NET} \
 --publish 9000:9000 \
---mount type=volume,src=${VOLUMES[2]},dst=/nextcloud \
---mount type=volume,src=${VOLUMES[3]},dst=/config \
---mount type=volume,src=${VOLUMES[4]},dst=/data \
---mount type=volume,src=${VOLUMES[5]},dst=/apps \
+--mount type=volume,src=${VOLUMES[1]},dst=/nextcloud \
+--mount type=volume,src=${VOLUMES[2]},dst=/config \
+--mount type=volume,src=${VOLUMES[3]},dst=/data \
+--mount type=volume,src=${VOLUMES[4]},dst=/apps \
 --env DB_TYPE=pgsql \
 --env DB_NAME=nextcloud \
 --env DB_USER=postgres \
 --env DB_PASSWORD=postgres \
---env DB_HOST=${SERVICES[1]} \
---env REDIS_HOST=${SERVICES[2]} \
---env MEMCACHE_HOST=${SERVICE[3]} \
+--env DB_HOST=${SERVICES[0]} \
+--env REDIS_HOST=${SERVICES[1]} \
+--env MEMCACHE_HOST=${SERVICE[2]} \
 --env ADMIN_USER=admin \
 --env ADMIN_PASSWORD=admin \
 --constraint 'node.hostname==sparrow' \
@@ -58,7 +61,8 @@ whatever4711/nextcloud:armhf
 }
 
 function create_ssh {
-docker service create --name ${SERVICES[5]}--replicas 1 --network ${NET} \
+  echo "Creating SSH service ${SERVICES[4]}}"
+docker service create --name ${SERVICES[4]}--replicas 1 --network ${NET} \
 --publish 2222:22 \
 --mount type=volume,src=nc_www,dst=/nextcloud \
 --env ROOT_PASS=12wert45 \
