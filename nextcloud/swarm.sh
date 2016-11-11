300ce5406674182ce0cb43d1c1fe6d782d572f78
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2016-11-11 14:09:52 +0100

    Swarm updates

--- nextcloud/swarm.sh
+++ nextcloud/swarm.sh
@@ -1,46 +1,84 @@
 #!/bin/bash
 
-docker network create --driver overlay cloud_net_internal
-docker network create --driver overlay cloud_net_external
+NET=cloud_net
+SHARE_HOST=sparrow.mg
+SHARE_DIR=/home/marcel/clouddata/Wedding
+SHARE=${SHARE_HOST}:${SHARE_DIR}
+VOLUMES=('nc_db' 'nc_www' 'nc_config' 'nc_data' 'nc_apps')
+SERVICES=('cloud_postgres' 'cloud_redis' 'cloud_memcache' 'cloud_nextcloud' 'cloud_ssh')
 
-docker volume create -d nfs --name pg_data -o share=sparrow.mg:/home/marcel/clouddata/Wedding/pg_data
 
-docker service create --name cloud_postgres --replicas 1 --network cloud_net_internal \
---mount type=volume,src=pg_data,dst=/var/lib/postgresql/data \
+function create() {
+  docker network create --driver overlay ${NET}
+  for volume in ${VOLUMES} do
+    docker volume create -d nfs --name ${volume} -o share=${SHARE}/${volume}
+  done
+}
+
+function createDB() {
+docker service create --name ${SERVICES[1]} --replicas 1 --network ${NET} \
+--mount type=volume,src=${VOLUMES[1]},dst=/var/lib/postgresql/data \
 --constraint 'node.hostname==sparrow' \
 whatever4711/postgres:armhf
+}
 
-docker service create --name cloud_redis --replicas 1 --network cloud_net_internal \
+function createRedis() {
+docker service create --name ${SERVICES[2]} --replicas 1 --network ${NET} \
+--constraint 'node.hostname!=jack' \
 armhf/redis
+}
 
-docker service create --name cloud_memcache --replicas 1 --network cloud_net_internal \
+function createMemcache() {
+docker service create --name ${SERVICE[3]} --replicas 1 --network ${NET} \
+--constraint 'node.hostname!=jack' \
 armhf/memcached:alpine
+}
 
-docker volume create -d nfs --name nc_www -o share=sparrow.mg:/home/marcel/clouddata/Wedding/nc_www
-docker volume create -d nfs --name nc_config -o share=sparrow.mg:/home/marcel/clouddata/Wedding/nc_config
-docker volume create -d nfs --name nc_data -o share=sparrow.mg:/home/marcel/clouddata/Wedding/nc_data
-docker volume create -d nfs --name nc_apps -o share=sparrow.mg:/home/marcel/clouddata/Wedding/nc_apps
-
-docker service create --name cloud_nextcloud --replicas 1 --network cloud_net_internal \
+function create nextCloud() {
+docker service create --name ${SERVICES[4]} --replicas 1 --network ${NET} \
 --publish 9000:9000 \
---mount type=volume,src=nc_www,dst=/nextcloud \
---mount type=volume,src=nc_config,dst=/config \
---mount type=volume,src=nc_data,dst=/data \
---mount type=volume,src=nc_apps,dst=/apps \
+--mount type=volume,src=${VOLUMES[2]},dst=/nextcloud \
+--mount type=volume,src=${VOLUMES[3]},dst=/config \
+--mount type=volume,src=${VOLUMES[4]},dst=/data \
+--mount type=volume,src=${VOLUMES[5]},dst=/apps \
 --env DB_TYPE=pgsql \
 --env DB_NAME=nextcloud \
 --env DB_USER=postgres \
 --env DB_PASSWORD=postgres \
---env DB_HOST=cloud_postgres \
---env REDIS_HOST=cloud_redis \
---env MEMCACHE_HOST=cloud_memcache \
+--env DB_HOST=${SERVICES[1]} \
+--env REDIS_HOST=${SERVICES[2]} \
+--env MEMCACHE_HOST=${SERVICE[3]} \
 --env ADMIN_USER=admin \
 --env ADMIN_PASSWORD=admin \
 --constraint 'node.hostname==sparrow' \
 whatever4711/nextcloud:armhf
+}
 
-docker service create --name cloud_ssh --replicas 1 --network cloud_net_internal --network cloud_net_external \
+function create_ssh() {
+docker service create --name ${SERVICES[5]}--replicas 1 --network ${NET} \
 --publish 2222:22 \
 --mount type=volume,src=nc_www,dst=/nextcloud \
---constraint 'node.hostname==sparrow' \
+--env ROOT_PASS=12wert45 \
+--constraint 'node.hostname!=jack' \
 whatever4711/ssh:armhf
+}
+
+function start() {
+  create
+  createDB
+  createRedis
+  createMemcache  
+}
+
+function destroy() {
+  for service in ${SERVICES} do
+    docker service rm ${service}
+  done
+
+  for volume in ${VOLUMES} do
+    docker volume rm ${volume}
+  done
+
+  docker network rm ${NET}
+
+}
