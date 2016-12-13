d68e83ff99101eca539831b803a023f0cf2c3b6a
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2016-12-13 12:13:09 +0100

    Swarm Updates

--- nextcloud/swarm.sh
+++ nextcloud/swarm.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 NET='cloud_net'
-SHARE_HOST_1='cubietruck.mg'
-SHARE_HOST_2='pine64.mg'
-SHARE_DIR_1='/home/marcel/clouddata/Wedding'
-SHARE_DIR_2='/home/marcel/clouddata/Wedding'
-SHARE_1=${SHARE_HOST_1}:${SHARE_DIR_1}
-SHARE_2=${SHARE_HOST_2}:${SHARE_DIR_2}
+SHARE_HOST_1='192.168.9.1'
+SHARE_HOST_2='192.168.9.250'
+SHARE_DIR='/home/marcel/clouddata/Wedding'
+RESTART_DELAY='10s'
+RESTART_ATTEMPTS='5'
 VOLUMES_1=( 'nc_db' 'nc_www' 'nc_config' )
 VOLUMES_2=( 'nc_data' 'nc_apps' )
 VOLUMES=( "${VOLUMES_1[@]}" "${VOLUMES_2[@]}" )
@@ -55,28 +54,15 @@ function setServices {
 function createBasics {
 echo "Creating network ${NET}"
 docker network create --driver overlay --subnet=192.168.111.0/24 ${NET}
-for volume in "${VOLUMES_1[@]}"; do
-	echo "Creating volume ${volume}"
-	docker volume create -d nfs --name ${volume} -o share=${SHARE_1}/${volume}
-done
-for volume in "${VOLUMES_2[@]}"; do
-	echo "Creating volume ${volume}"
-	docker volume create -d nfs --name ${volume} -o share=${SHARE_2}/${volume}
-done
-ssh cubietruck.mg "docker volume create -d nfs --name ${VOLUMES[0]} -o share=${SHARE_1}/${VOLUMES[0]}"
-docker node update --label-add ${VOLUMES[0]} cubietruck
-ssh neo.mg "docker volume create -d nfs --name ${VOLUMES[1]} -o share=${SHARE_1}/${VOLUMES[1]}"
-docker node update --label-add nextcloud=${VOLUMES[1]} neo
-docker node update --label-add nextcloud=${VOLUMES[1]} sparrow
 }
 
 function createDB {
 local db=${1:-cloud_postgres}
 echo "Creating DB service ${db} with volume ${VOLUMES[0]}"
 docker service create --name ${db} --replicas 1 --network ${NET} \
+  --restart-delay ${RESTART_DELAY} --restart-max-attempts ${RESTART_ATTEMPTS} \
 	--publish 5432:5432 \
-	--mount type=volume,src=${VOLUMES[0]},dst=/var/lib/postgresql/data \
-	--constraint 'node.hostname==cubietruck' \
+  --mount type=volume,volume-opt=o=addr="${SHARE_HOST_1}",volume-opt=device="${SHARE_DIR}"/${VOLUMES[0]},volume-opt=type=nfs,src=${VOLUMES[0]},dst=/var/lib/postgresql/data \
 	whatever4711/postgres:armhf
 SERVICES+=("${db}")
 }
@@ -85,9 +71,10 @@ function createRedis {
 local redis=${1:-cloud_redis}
 echo "Creating Redis service ${redis}"
 docker service create --name ${redis} --replicas 1 --network ${NET} \
+  --restart-delay ${RESTART_DELAY} --restart-max-attempts ${RESTART_ATTEMPTS} \
 	--publish 6379:6379 \
 	--mount type=bind,src=${PWD}/config/redis.conf,dst=/redis.conf \
-	--constraint 'node.hostname!=roupi' --constraint 'node.hostname!=jack' \
+	--constraint 'node.hostname!=jack' \
 	armhf/redis redis-server /redis.conf
 SERVICES+=("${redis}")
 }
@@ -97,9 +84,9 @@ local memcache=${1:-cloud_memcache}
 local publishedPort=${2:-11211}
 echo "Creating Memcache service ${memcache}"
 docker service create --name ${memcache} --replicas 1 --network ${NET} \
+  --restart-delay ${RESTART_DELAY} --restart-max-attempts ${RESTART_ATTEMPTS} \
 	--publish ${publishedPort}:11211 \
-	--constraint 'node.hostname!=roupi' \
-	armhf/memcached:alpine -m 256
+	armhf/memcached:alpine -m 128
 SERVICES+=("${memcache}")
 }
 
@@ -107,21 +94,21 @@ function createNextCloud {
 local nextcloud=${1:-cloud_nextcloud}
 echo "Creating Nextcloud service ${nextcloud} with volumes ${VOLUMES[1]}, ${VOLUMES[2]}, ${VOLUMES[3]}, and ${VOLUMES[4]}"
 docker service create --name ${nextcloud} --replicas 1 --network ${NET} \
+  --restart-delay ${RESTART_DELAY} --restart-max-attempts ${RESTART_ATTEMPTS} \
 	--publish 9000:9000 \
-	--mount type=volume,src=${VOLUMES[1]},dst=/nextcloud \
-	--mount type=volume,src=${VOLUMES[2]},dst=/config \
-	--mount type=volume,src=${VOLUMES[3]},dst=/data \
-	--mount type=volume,src=${VOLUMES[4]},dst=/apps \
+	--mount type=volume,volume-opt=o=addr="${SHARE_HOST_1}",volume-opt=device="${SHARE_DIR}"/${VOLUMES[1]},volume-opt=type=nfs,src=${VOLUMES[1]},dst=/nextcloud \
+	--mount type=volume,volume-opt=o=addr="${SHARE_HOST_1}",volume-opt=device="${SHARE_DIR}"/${VOLUMES[2]},volume-opt=type=nfs,src=${VOLUMES[2]},dst=/config \
+	--mount type=volume,volume-opt=o=addr="${SHARE_HOST_2}",volume-opt=device="${SHARE_DIR}"/${VOLUMES[3]},volume-opt=type=nfs,src=${VOLUMES[3]},dst=/data \
+	--mount type=volume,volume-opt=o=addr="${SHARE_HOST_2}",volume-opt=device="${SHARE_DIR}"/${VOLUMES[4]},volume-opt=type=nfs,src=${VOLUMES[4]},dst=/apps \
 	--env DB_TYPE=pgsql \
 	--env DB_NAME=nextcloud \
 	--env DB_USER=postgres \
 	--env DB_PASSWORD=postgres \
 	--env DB_HOST=192.168.9.3 \
 	--env REDIS_HOST=192.168.9.3 \
-	--env MEMCACHE_ARRAY="array('192.168.9.3', 11211), array('192.168.9.250', 11212)" \
+	--env MEMCACHE_ARRAY="array('192.168.9.3', 11211)" \
 	--env ADMIN_USER=admin \
 	--env ADMIN_PASSWORD=admin \
-	--constraint 'node.hostname==sparrow' \
 	whatever4711/nextcloud:armhf
 SERVICES+=("${nextcloud}")
 }
@@ -130,10 +117,10 @@ function createCaddy {
 	local caddy=${1:-cloud_caddy}
 	echo "Creating Caddy service ${caddy} with volumes "
 	docker service create --name ${caddy} --replicas 1 --network ${NET} \
+	  --restart-delay ${RESTART_DELAY} --restart-max-attempts ${RESTART_ATTEMPTS} \
 		--publish 80:80 \
-		--mount type=volume,src=${VOLUMES[1]},dst=/nextcloud \
+		--mount type=volume,volume-opt=o=addr="${SHARE_HOST_1}",volume-opt=device="${SHARE_DIR}"/${VOLUMES[1]},volume-opt=type=nfs,src=${VOLUMES[1]},dst=/nextcloud \
 		--mount type=bind,src=${PWD}/config/Caddyfile,dst=/root/.caddy/Caddyfile \
-		--constraint "node.labels.nextcloud==${VOLUMES[1]}" \
 		whatever4711/caddy:armhf --agree --conf /root/.caddy/Caddyfile
 	SERVICES+=("${caddy}")
 }
@@ -142,6 +129,7 @@ function create_ssh {
 local ssh=${1:-cloud_ssh}
 echo "Creating SSH service ${ssh}"
 docker service create --name ${ssh} --replicas 1 --network ${NET} \
+  --restart-delay ${RESTART_DELAY} --restart-max-attempts ${RESTART_ATTEMPTS} \
 	--publish 2222:22 \
 	--env ROOT_PASS=12wert45 \
 	--constraint 'node.hostname!=roupi' \
@@ -151,11 +139,9 @@ SERVICE+=("${ssh}")
 
 function create {
 createBasics
-sleep 10
 createDB
 createRedis
 createMemcache
-#createMemcache cloud_memcache_2 11212
 setServices
 }
 
@@ -178,7 +164,7 @@ for service in "${SERVICES[@]}"; do
 	docker service rm ${service}
 done
 sleep 10
-
+# TODO: On Each node!
 for volume in "${VOLUMES[@]}"; do
 	docker volume rm ${volume}
 done
@@ -195,7 +181,7 @@ create       create
 start        start
 debug        debugging with ssh container
 destroy      destroy
-reset        reset
+reset        reset - Use with sudo and care
 
 EOM
 }
