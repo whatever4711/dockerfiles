891b146a17754340ee336b6368aed2df72f4a204
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2017-09-27 10:44:59 +0200

    Updates

--- miner/start.sh
+++ miner/start.sh
@@ -2,7 +2,7 @@
 proxy=proxy:9050
 sha_pool=stratum+tcp://eu.multipool.us:8888 
 scrypt_pool=stratum+tcp://ltc.pool.minergate.com:3336 
-
+usb=$(lsusb |grep "0483:5740"|cut -d " " -f 2,4|sed 's/://g'|sed 's/ /:/g')
 while getopts b:l:p: option 
 do
 	case "${option}" in
@@ -12,6 +12,7 @@ do
 	esac
 done
 
-cgminer --gridseed-options=baud115200,freq=800,chips=5,modules=1,usefifo=0,btc=16 --hotplug=0 -o ${sha_pool} -u whatever4711.1 -p x --socks-proxy="${proxy}" --usb 001:022  -T &
+echo "Starting cminer on USB ${usb}"
+cgminer --gridseed-options=baud115200,freq=800,chips=5,modules=1,usefifo=0,btc=16 --hotplug=0 -o ${sha_pool} -u whatever4711.1 -p x --socks-proxy="${proxy}" --usb ${usb}  -T &
   
 minerd --algo=scrypt -F 800 -G /dev/ttyACM0 -o ${scrypt_pool} -u whatever4711@gmail.com -p x --proxy="socks5://${proxy}" --dual
