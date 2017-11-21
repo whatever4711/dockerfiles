95e8ce031f6ac1fc03e86028e8465e081df9ac7f
Marcel Grossmann <whatever4711@gmail.com>
2017-11-21 17:51:01 +0100

    Without Debug

--- miner/start.sh
+++ miner/start.sh
@@ -5,7 +5,9 @@ sha_user=whatever4711.1
 scrypt_pool=stratum+tcp://ltc.pool.minergate.com:3336
 scrypt_user=whatever4711@gmail.com
 usb=$(lsusb |grep "0483:5740"|cut -d " " -f 2,4|sed 's/://g'|sed 's/ /:/g')
-tty=$(ls -l /dev/tty* | grep "dialout" | cut -d " " -f 11)
+tty=/dev/ttyACM0
+# Not working in docker
+#tty=$(ls -l /dev/tty* | grep "dialout" | cut -d " " -f 11)
 while getopts b:c:l:m:p: option 
 do
 	case "${option}" in
@@ -21,7 +23,7 @@ echo "Starting cminer on USB ${usb}"
 echo "With SHA-Pool ${sha_pool}"
 for miner in ${usb};
 do
-	cgminer --gridseed-options=baud115200,freq=800,chips=5,modules=1,usefifo=0,btc=16 --hotplug=0 -o ${sha_pool} -u ${sha_user} -p x --socks-proxy="${proxy}" --usb ${miner} -T -D &
+	cgminer --gridseed-options=baud115200,freq=800,chips=5,modules=1,usefifo=0,btc=16 --hotplug=0 -o ${sha_pool} -u ${sha_user} -p x --socks-proxy="${proxy}" --usb ${miner} -T &
 done
 echo "Starting minerd on ${tty}"
 echo "With Scrypt-Pool ${scrypt_pool}"
